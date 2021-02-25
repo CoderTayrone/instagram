dbc0326fd0da7bf9c3f929678d1827a9fd282750
wesley guerra <guerra_ssa@hotmail.com>
2021-02-25 20:29:31 -0300

    criando contexto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import PageProfile from '../src/Pages/Profile';
 import Feed from '../src/Pages/Feed';
 import Nav from '../src/Components/Nav';
-
-
+import ResearchProvider from './context/ResearchContext';
 
 function App() {
   return (
-    <>
+    <ResearchProvider>
+      <Router>
+        <Nav />
+        <Switch>
+          <Route path="/" exact component={Feed} />
+          {/* <Route path="/Profile" exact component={PageProfile}/> */}
+          <Route path="/Profile/:name" exact component={PageProfile} />
 
-            <Router>
-              <Nav/>
-                <Switch>
-                    <Route path="/" exact component={Feed}/>
-                    {/* <Route path="/Profile" exact component={PageProfile}/> */}
-                    <Route path="/Profile/:name" exact component={PageProfile}/>
-                    
-                </Switch>
-            </Router>
-    </>
+        </Switch>
+      </Router>
+    </ResearchProvider>
   );
 }
 
--- src/Pages/Profile/index.js
+++ src/Pages/Profile/index.js
@@ -2,31 +2,34 @@ import '../../../src/style.css';
 import React, { useState } from 'react';
 import CardP from '../../Components/CardPost';
 import CardD from '../../Components/CardDestaque';
-import {Container, ContainerHeader, ImgLogo, ContentProfile, ContainerInsight, ContainerDestaques, ContainerTitles, ContainerPostagens, ContainerModal} from '../Profile/style';
+import { Container, ContainerHeader, ImgLogo, ContentProfile, ContainerInsight, ContainerDestaques, ContainerTitles, ContainerPostagens, ContainerModal } from '../Profile/style';
 import posts from '../../Config/post';
 import profiles from '../../Config/profiles';
 import imgProfile from '../../images/imgProfile-150x150.jpg';
 import imgStorie from '../../images/imgPost-640x640.jpg';
 import '../../Components/Nav';
+import { useContext } from "react";
+import ResearchContext from '../../context/ResearchContext';
 
 function Profile() {
-/* 
-    const [profiles, setProfiles] = useState();
- */
+    const { search } = useContext(ResearchContext);
+    /* 
+        const [profiles, setProfiles] = useState();
+     */
     const [destaques, setDestaques] = useState([
-        {   
+        {
             idDestaque: 1,
             imgDestaque: imgStorie
         },
-        {   
+        {
             idDestaque: 2,
             imgDestaque: imgStorie
         },
-        {   
+        {
             idDestaque: 3,
             imgDestaque: imgStorie
         },
-        {   
+        {
             idDestaque: 4,
             imgDestaque: imgStorie
         }
@@ -34,31 +37,31 @@ function Profile() {
 
     let perfils = profiles.filter((profile) => {
         return profile.nameProfile === search;
-      })
+    })
 
     /* const [posts, setPosts] = useState(); */
 
     const [modalIsOpen, setModalIsOpen] = useState(false);
-    
+
     return (
-      <>
-          <Container>
-                
-                {   
+        <>
+            <Container>
+
+                {
                     perfils.map(profile => (
                         <ContainerHeader> {/* Container Header */}
-                    
+
                             <ImgLogo> {/* Profile Header */}
-                                <img src={profile.imgProfile}></img> {/* Profile Header */} 
-                            </ImgLogo> 
-                            
+                                <img src={profile.imgProfile}></img> {/* Profile Header */}
+                            </ImgLogo>
+
                             <ContentProfile> {/* Content Profile Header */}
                                 <h2>{profile.nameProfile}</h2>
-                                    <ContainerInsight>
-                                        <p><b>{profile.nPublicacoes}</b> Publicações</p>
-                                        <p><b>{profile.nSeguires}</b> Seguidores</p>
-                                        <p><b>{profile.nSeguindo}</b> Seguindo</p>
-                                    </ContainerInsight>
+                                <ContainerInsight>
+                                    <p><b>{profile.nPublicacoes}</b> Publicações</p>
+                                    <p><b>{profile.nSeguires}</b> Seguidores</p>
+                                    <p><b>{profile.nSeguindo}</b> Seguindo</p>
+                                </ContainerInsight>
                                 <h3>{profile.title}</h3>
                                 <p>{profile.describle_n1}</p>
                                 <p>{profile.describle_n2}</p>
@@ -66,21 +69,21 @@ function Profile() {
                                 <p>{profile.describle_n4}</p>
 
                             </ContentProfile>
-                        
+
                         </ContainerHeader>
                     ))
                 }
 
                 <ContainerDestaques>{/* Container Destaques */}
 
-                        {
-                            destaques.map( destaque => (
-                                <div>{/* <img src={destaque.imgDestaque}></img> */}
-                                <CardD key={destaque.idDestaque} img={destaque.imgDestaque}></CardD> 
-                                
-                                </div>
-                            ))
-                        }                        
+                    {
+                        destaques.map(destaque => (
+                            <div>{/* <img src={destaque.imgDestaque}></img> */}
+                                <CardD key={destaque.idDestaque} img={destaque.imgDestaque}></CardD>
+
+                            </div>
+                        ))
+                    }
 
                 </ContainerDestaques>
                 <hr></hr>
@@ -91,18 +94,18 @@ function Profile() {
                     <p>MARCADOS</p>
                 </ContainerTitles>
                 <ContainerPostagens>{/* Container Postagens */}
-                
+
                     {
-                           posts.map(post => (
-                             <CardP key={post.idPost} img={post.imgPost} countLikes={post.countLikes} countMessages={post.countMessages}></CardP>  
-                        ))   
-                    }                      
-                    
+                        posts.map(post => (
+                            <CardP key={post.idPost} img={post.imgPost} countLikes={post.countLikes} countMessages={post.countMessages}></CardP>
+                        ))
+                    }
+
                 </ContainerPostagens>
-                
-          </Container>
-      </>
+
+            </Container>
+        </>
     );
-  }
-  
-  export default Profile;
+}
+
+export default Profile;
